7bcabef59e1b204a30d674e6fc8383b33ae9df97
Mauricio Fernandez <mfp@acm.org>
2007-01-29 12:13:40 +0100

    lib/fastri/version.rb: set FASTRI_RELEASE_DATE.
    darcs-hash:20070129111340-0786a-35d8863461b4711eddbba6e0ab3c6926c57af8bf.gz

--- lib/fastri/version.rb
+++ lib/fastri/version.rb
@@ -3,7 +3,7 @@
 
 module FastRI
   FASTRI_VERSION      = "0.3.0"
-  FASTRI_RELEASE_DATE = "unreleased (internal/preliminary)"
+  FASTRI_RELEASE_DATE = "2007-01-29"
   FASTRI_INDEX_FORMAT = "0.1.0"
   FASTRI_FT_INDEX_FORMAT = "1.0.0"
   FASTRI_FT_INDEX_FORMAT_MAJOR = "1"
